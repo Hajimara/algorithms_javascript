0ae140876eeed21180ea4af21512b585188c4c31
ParkSujin <show3rg@gmail.com>
2024-07-24 10:18:51 +0900

    save

---
+++ section-6/max-subarray-sum.js
@@ -0,0 +1,12 @@
+// Sliding Window - maxSubarraySum
+// 정수의 배열과 숫자가 주어졌을 때, 함수에 전달된 숫자의 길이를 가진 하위 배열의 최대 합을 구하는 maxSubarraySum이라는 함수를 작성하세요.
+// 하위 배열은 원래 배열의 연속적인 요소로 구성되어야 한다는 점에 유의하세요. 아래 첫 번째 예제에서 [100, 200, 300]은 원래 배열의 하위 배열이지만 [100, 300]은 그렇지 않습니다.
+function maxSubarraySum() {}
+
+const start = performance.now();
+console.log(maxSubarraySum(182, 281));
+console.log(maxSubarraySum(34, 14));
+console.log(maxSubarraySum(3589578, 5879385));
+console.log(maxSubarraySum(22, 222));
+
+const end = performance.now();
